a27cb7e65d38edb6819cc1bf6105b75a9e8de99d
Nataliia Leshchak <n.leshchak@ualwo8689nbl.levi9.com>
2022-05-24 14:07:19 +0300

    guess app

---
+++ components/ui/Card.js
@@ -0,0 +1,29 @@
+import { View, StyleSheet } from "react-native";
+import Colors from "../../constants/colors";
+
+const Card = ({ children }) => {
+  return <View style={styles.card}>{children}</View>;
+};
+
+export default Card;
+
+const styles = StyleSheet.create({
+  card: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginHorizontal: 24,
+    borderRadius: 8,
+    padding: 16,
+    marginTop: 100,
+    backgroundColor: Colors.primary800,
+    elevation: 4, // Android
+    shadowColor: "black", // iOS
+    shadowOffset: {
+      // iOS
+      width: 0,
+      height: 2,
+    },
+    shadowRadius: 6, // iOS
+    shadowOpacity: 0.25, // iOS
+  },
+});
